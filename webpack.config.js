77144e2f2a9a73cc5b478ccbbaf6d342873a2b94
Anbeeld <anbeeld@gmail.com>
2025-01-13 11:19:31 +0100

    Fixed indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 import path from 'path';
 
 const webConfig = {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    extensionAlias: {
+      '.js': ['.js', '.ts'],
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        extensionAlias: {
-            '.js': ['.js', '.ts'],
-        },
+  },
+  target: 'web',
+  entry: './index.ts',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'raoi.js',
+    library: {
+      name: 'Raoi',
+      type: 'var',
+      export: 'default',
     },
-    target: 'web',
-    entry: './index.ts',
-    output: {
-        path: path.resolve('./dist'),
-        filename: 'raoi.js',
-        library: {
-            name: 'Raoi',
-            type: 'var',
-            export: 'default',
-        },
-    },
+  },
 };
 
 export default [webConfig];
